5cb4b5b30f4c98e212c2d5b8946c24b26261eb14
Su Mon Aung <sumonaung575@gmail.com>
2023-08-09 10:10:55 +0630

    wrapped up weather-app

---
+++ weather-app/index.js
@@ -0,0 +1,48 @@
+const apiKey = 'eaf31e603f683097354d0a5b6a048af4';
+        const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+        const searchBox = document.querySelector('.search input');
+        const searchBtn = document.querySelector('.search button');
+
+        async function checkWeather(city) {
+            const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+            
+            if (response.status === 404) {
+                document.querySelector('.error').style.display = 'block';
+                document.querySelector('.weather').style.display = 'none';
+            } else {
+                let data = await response.json();
+
+                console.log(data);
+
+                document.querySelector('.city').innerHTML = data.name;
+                document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+                document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+                document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
+
+                const weatherIcon = document.querySelector('.weather-icon');
+                
+                if (data.weather[0].main == 'Clouds') {
+                    weatherIcon.src = 'images/clouds.png';
+                } else if (data.weather[0].main == 'Clear') {
+                    weatherIcon.src = 'images/clear.png';
+                } else if (data.weather[0].main == 'Rain') {
+                    weatherIcon.src = 'images/rain.png';
+                } else if (data.weather[0].main == 'Drizzle') {
+                    weatherIcon.src = 'images/drizzle.png';
+                } else if (data.weather[0].main == 'Mist') {
+                    weatherIcon.src = 'images/mist.png';
+                }
+
+                document.querySelector('.weather').style.display = 'block'; 
+                document.querySelector('.error').style.display = 'none';
+            }
+        }
+
+        searchBtn.addEventListener('click', () => {
+            const cityName = searchBox.value;
+            checkWeather(cityName);
+        });
+
+        // Initial weather check (you might want to provide a default city)
+        // For example: checkWeather('New York');
